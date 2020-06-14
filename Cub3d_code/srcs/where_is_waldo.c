a3a8f982a69d20b92a78ff0e657d0afbb00241f7
Mylhow <dgascon@student.42lyon.fr>
2020-06-14 11:51:38 +0200

    No Header

--- Cub3d_code/srcs/where_is_waldo.c
+++ Cub3d_code/srcs/where_is_waldo.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   where_is_waldo.c                                   :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: aducas <aducas@student.le-101.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/03/10 13:30:04 by aducas            #+#    #+#             */
-/*   Updated: 2020/06/14 11:14:24 by user42           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "../includes/cub3d.h"
 
 int		chr_player(char **map_ok, int y, int x)
@@ -41,11 +29,7 @@ int		find_player(char **map_ok, t_cub3d *cub3d)
 	x = 0;
 	y = 0;
 	while ((x = ft_strchrstr(map_ok[y], "NSEW")) == 0
-<<<<<<< HEAD
 		&& map_ok[y] != '\0')
-=======
-		&& map_ok[y + 1] != '\0')
->>>>>>> fb8175628eb35a9aed63fce8aeb43b8e62db1250
 		y++;
 	if (!x)
 	{
